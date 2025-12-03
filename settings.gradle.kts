13f03d8fc7a198f646078aa02ce37902f4c59f36
yhjun0523 <windofwish@gmail.com>
2025-12-03 21:38:45 +0900

    feat: add pg-simulator

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ rootProject.name = "loopers-java-spring-template"
 include(
     ":apps:commerce-api",
     ":apps:commerce-streamer",
+    ":apps:pg-simulator",
     ":modules:jpa",
     ":modules:redis",
     ":modules:kafka",
